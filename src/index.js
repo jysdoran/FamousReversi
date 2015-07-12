17fd6a54db1448e7b5f421c11ccabd089adc6c75
James Doran <jysdoran@gmail.com>
2015-07-12 16:55:40 +0800

    Add Skip Button
    WIP Undo functionality

--- src/index.js
+++ src/index.js
@@ -17,17 +17,64 @@ FamousEngine.init();
 var scene = FamousEngine.createScene();
 var boardNode = scene.addChild();
 
+//Creation of player info div node
+var info = scene.addChild();
+info.setSizeMode('absolute', 'absolute', 'absolute')
+	.setPosition(200, 100, 0)
+	.setMountPoint(0, 0, 0)
+	.setAbsoluteSize(200, 50,0);
+
+var infodiv = new DOMElement(info, { tagName: 'div' });
+
+/*Unfinished Undo button
+var undo = scene.addChild();
+
+undo.setSizeMode('absolute', 'absolute', 'absolute')
+	.setPosition(400, 100, 0)
+	.setMountPoint(0, 0, 0)
+	.setAbsoluteSize(50, 50,0);
+
+var undoimg = new DOMElement(undo, { tagName: 'img' });
+undoimg.setAttribute('src', './images/undo.png');
+
+undo.addUIEvent('click');
+undoimg.on('click', restoreBoard);
+*/
+var skipnode = scene.addChild();
+
+skipnode.setSizeMode('absolute', 'absolute', 'absolute')
+	.setPosition(450, 100, 0)
+	.setMountPoint(0, 0, 0)
+	.setAbsoluteSize(50, 50,0);
+
+var skipimg = new DOMElement(skipnode, { tagName: 'img' });
+skipimg.setAttribute('src', './images/skip.png');
+
+skipnode.addUIEvent('click');
+skipimg.on('click', function(){
+	if (currentPlayer == 'black') {
+    	currentPlayer = 'white';
+    } else {
+    	currentPlayer = 'black';
+    }
+});
+
 //Global 'tweak' variables
 var period = 1000;
 var size = 40;
 var buffer = 4;
 var boardPosition = 200;
 var boardSize = 8;
-var easingcurve = 'outBounce'
+var easingcurve = 'linear';
+var pi = 3.14;
 
-
+//Working variables
 var currentPlayer = 'black';
 var board = [[]];
+var oldBoard = [[]];
+var wcount = 0;
+var bcount = 0;
+
 
 function Counter(x, y, state) {
 	this.node = scene.addChild();
@@ -35,7 +82,7 @@ function Counter(x, y, state) {
 	this.x = x;
 	this.y = y;
 
-	this.xrotationpos = new Transitionable(0);
+	this.xrotationpos = new Transitionable(0); //Trasitionables allow the use of easing curves
 	this.lastXRotation = 0;
 	this.currentXRotation = 0;
 	this.yrotationpos = new Transitionable(0);
@@ -43,7 +90,14 @@ function Counter(x, y, state) {
 	this.currentYRotation = 0;
 
 	this.state = state;
+	if (this.state == 'black') {
+		bcount++;
+	} else{
+		wcount++;
+	}
+
 	this.image = new DOMElement(this.node, { tagName: 'img' });
+	//Dirty way of reusing image assignment code
 	this.toggle();
 	this.toggle();
 
@@ -62,38 +116,40 @@ Counter.prototype.onUpdate = function onUpdate(time) {
 	this.currentXRotation = this.xrotationpos.get()
 	this.currentYRotation = this.yrotationpos.get()
 
-	if (Math.abs(this.currentXRotation) == 6.28) {
+	//Dirty fix for counter flipping
+	if (Math.abs(this.currentXRotation) == 2 * pi) {
 		this.xrotationpos.set(0);
 		this.currentXRotation = 0;
 		this.lastXRotation = 0;
-	} else if (Math.abs(this.currentXRotation) == 3.14) {
+	} else if (Math.abs(this.currentXRotation) == pi) {
 		this.lastXRotation = this.currentXRotation;
 	}
-	if (Math.abs(this.currentYRotation) == 6.28) {
+	if (Math.abs(this.currentYRotation) == 2 * pi) {
 		this.yrotationpos.set(0);
 		this.currentYRotation = 0;
 		this.lastYRotation = 0;
-	} else if (Math.abs(this.currentYRotation) == 3.14) {
+	} else if (Math.abs(this.currentYRotation) == pi) {
 		this.lastYRotation = this.currentYRotation;
 	}
 
 	//Horrible and dirty check for flipping over the counter
-	if (Math.abs(this.lastXRotation) != 3.14 &&
-			((this.lastXRotation % 3.14 < 1.57 && this.currentXRotation % 3.14 > 1.57) || 
-			(this.lastXRotation % 3.14 > 1.57 && this.currentXRotation % 3.14 < 1.57) ||
-			(this.lastXRotation % 3.14 > -1.57 && this.currentXRotation % 3.14 < -1.57) ||
-			(this.lastXRotation % 3.14 < -1.57 && this.currentXRotation % 3.14 > -1.57))) {
+	if (Math.abs(this.lastXRotation) != pi &&
+			((this.lastXRotation % pi < pi/2 && this.currentXRotation % pi > pi/2) || 
+			(this.lastXRotation % pi > pi/2 && this.currentXRotation % pi < pi/2) ||
+			(this.lastXRotation % pi > -pi/2 && this.currentXRotation % pi < -pi/2) ||
+			(this.lastXRotation % pi < -pi/2 && this.currentXRotation % pi > -pi/2))) {
 		this.toggle();
-	} else if (Math.abs(this.lastYRotation) != 3.14 && 
-			((this.lastYRotation % 3.14 < 1.57 && this.currentYRotation % 3.14 > 1.57) ||
-			(this.lastYRotation % 3.14 > 1.57 && this.currentYRotation % 3.14 < 1.57) ||
-			(this.lastYRotation % 3.14 > -1.57 && this.currentYRotation % 3.14 < -1.57) ||
-			(this.lastYRotation % 3.14 < -1.57 && this.currentYRotation % 3.14 > -1.57))) {
+	} else if (Math.abs(this.lastYRotation) != pi && 
+			((this.lastYRotation % pi < pi/2 && this.currentYRotation % pi > pi/2) ||
+			(this.lastYRotation % pi > pi/2 && this.currentYRotation % pi < pi/2) ||
+			(this.lastYRotation % pi > -pi/2 && this.currentYRotation % pi < -pi/2) ||
+			(this.lastYRotation % pi < -pi/2 && this.currentYRotation % pi > -pi/2))) {
 		this.toggle();
 	}
-	
+	//Saving rotation
 	this.lastXRotation = this.currentXRotation;
 	this.lastYRotation = this.currentYRotation;
+	//Actual rotation assignment
 	this.node.setRotation(this.yrotationpos.get(), this.xrotationpos.get(), 0);
 	this.node.requestUpdateOnNextTick(this.id);
 };
@@ -102,7 +158,7 @@ Counter.prototype.flip = function (axis) {
 	var xflip = 1;
 	var yflip = 1;
 
-	switch (axis) {
+	switch (axis) { //So that counters always flip away from placed one
 		case 0: //Left
 			xflip = 1;
 			yflip = 0;
@@ -138,36 +194,42 @@ Counter.prototype.flip = function (axis) {
 	}
 
 
-
-	if (Math.abs(this.currentYRotation) == 3.14) {
+	//Fix for upside down counters
+	if (Math.abs(this.currentYRotation) == pi) {
 		xflip *= -1;
 	}
-	if (Math.abs(this.currentXRotation) == 3.14) {
+	if (Math.abs(this.currentXRotation) == pi) {
 		yflip *= -1;
 	}
 
 
-	this.xrotationpos.set(this.currentXRotation + (xflip * 3.14), { duration: period, curve: easingcurve });
-	this.yrotationpos.set(this.currentYRotation + (yflip * 3.14), { duration: period, curve: easingcurve });
+	this.xrotationpos.set(this.currentXRotation + (xflip * pi), { duration: period, curve: easingcurve });
+	this.yrotationpos.set(this.currentYRotation + (yflip * pi), { duration: period, curve: easingcurve });
 
 	this.lastXRotation = this.currentXRotation;
 	this.lastYRotation = this.currentYRotation;
-
 
 	console.log('Flip!');
 }
 
 Counter.prototype.toggle = function () {
-	console.log("X: "+ this.lastXRotation +", "+ this.currentXRotation);
-	console.log("Y: "+ this.lastYRotation +", "+ this.currentYRotation);
+	//console.log("X: "+ this.lastXRotation +", "+ this.currentXRotation);
+	//console.log("Y: "+ this.lastYRotation +", "+ this.currentYRotation);
 	
 	if (this.state == 'black') {
     	this.image.setAttribute('src', './images/famous_logo_i.png');
     	this.state = 'white';
+    	bcount--;
+    	wcount++;
 	} else {
     	this.image.setAttribute('src', './images/famous_logo.png');
     	this.state = 'black';
+    	bcount++;
+    	wcount--;
 	}
+
+	infodiv.setContent('White: ' + wcount + ', Black: ' + bcount);
+
 };
 
 function Tile(i,j) {
@@ -189,11 +251,15 @@ function Tile(i,j) {
 	this.node.i = i;
 	this.node.j = j;
 
+	//Neccesary for adding clicking functionality
 	this.node.addUIEvent('click');
 	this.domElement.on('click', placeCounter);
 }
 
 function placeCounter (e) {
+	/*for (var n = 0; n < board.length; n++) { //Make backup
+    	oldBoard[n] = board[n].slice();
+	}*/
 
 	var i = e.node.i;
 	var j = e.node.j;
@@ -208,7 +274,7 @@ function placeCounter (e) {
 		check[a] = [];
 		flip[a] = false;
 	}
-
+	//Check along cardinal directions until encountering same colour or empty tile
 	for (var a = i - 1; a >= 0; a--) { //Left
 		if (board[a][j].state != 'tile') {
 			if (board[a][j].state != currentPlayer) {
@@ -312,7 +378,7 @@ function placeCounter (e) {
 			a = boardSize;
 		}
 	}
-
+	//Flip all detected counters
 	for (var a = 0; a < 8; a++) {
 		if (flip[a] == true) {
 			for (var g = 0; g < check[a].length; g++) {
@@ -320,12 +386,13 @@ function placeCounter (e) {
 			}
 		}
 	}
-
+	//Toggle active player
 	if (currentPlayer == 'black') {
     	currentPlayer = 'white';
     } else {
     	currentPlayer = 'black';
     }
+
 };
 
 
@@ -338,6 +405,20 @@ function generateBoard(dim) {
     	}
 	}
 }
+/* Unfinished Undo Button
+function restoreBoard() {
+	console.log('Recieved!');
+	var temp = [[]];
+	for (var n = 0; n < board.length; n++) { //Make backup
+    	temp[n] = board[n].slice();
+	}
+	for (n = 0; n < oldBoard.length; n++) { //Make backup
+    	board[n] = oldBoard[n].slice();
+	}
+	for (n = 0; n < temp.length; n++) { //Make backup
+    	oldBoard[n] = temp[n].slice();
+	}
+}*/
 
 function getSign(num) {
 	return num/Math.abs(num);
@@ -345,9 +426,12 @@ function getSign(num) {
 
 //Actual code
 generateBoard(boardSize);
-
+//Starting grid
 board[3][3] = new Counter(board[3][3].x, board[3][3].y, 'black');
 board[3][4] = new Counter(board[3][4].x, board[3][4].y, 'white');
 board[4][4] = new Counter(board[4][4].x, board[4][4].y, 'black');
 board[4][3] = new Counter(board[4][3].x, board[4][3].y, 'white');
+
+infodiv.setContent('White: ' + wcount + ', Black: ' + bcount);
+
 console.log('Ready! -----------------------------------------------')
